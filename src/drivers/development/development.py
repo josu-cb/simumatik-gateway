ef19f9ff32fe3e3ee8c67e1afe00af76adcbd1c3
josu-cb <102325342+josu-cb@users.noreply.github.com>
2022-08-25 18:06:24 +0200

    Missing int(self.port) fix

--- src/drivers/development/development.py
+++ src/drivers/development/development.py
@@ -161,7 +161,7 @@ class development(driver):
     def loop(self):
         """ Runs every iteration while the driver is active."""
         if self.has_received and self._connection:
-            self._connection.sendto(json.dumps(self.variables_to_send).encode('utf8'), (self.ip, self.port))
+            self._connection.sendto(json.dumps(self.variables_to_send).encode('utf8'), (self.ip, int(self.port)))
             self.variables_to_send.clear()
             self.has_received = False
         
